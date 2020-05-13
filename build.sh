a531dc0f2244a39f87b881d9c4841b8804897984
大宝 <742393511@qq.com>
2020-05-13 15:53:02 +0800

    travis

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ git config --global user.email "742393511@qq.com"
 git config --global user.name "hellobili"
 git add --all .
 git commit -m "Travis CI Auto Builder" #自动构建后的内容将全部以此信息提交
-git push --quiet --force https://${GH_TOKEN}@github.com/hellobili/hellobili.github.io.git master  #自动构建后的内容将全部以此信息提交
+git push --quiet --force https://${GH_TOKEN}@github.com/hellobili/hellobili.github.io.git source  #自动构建后的内容将全部以此信息提交
 # curl --connect-timeout 20 --max-time 30 -s http://远端服务器URL/webhook.php?_=${NOTIFY_TOKEN} #服务器Webhook 将在下文介绍
